cb44a229838e70933188ec2dd69f0ec7ac2c9c23
wbyates777 <wbyates777@gmail.com>
2025-12-10 03:27:27 +0100

    BSpatialMatrix update constructors

--- BSpatialMatrix.h
+++ BSpatialMatrix.h
@@ -96,10 +96,16 @@ public:
                               } {}
     
     BSpatialMatrix( const BMatrix3 &m0, const BMatrix3 &m1, 
-                    const BMatrix3 &m2, const BMatrix3 &m3 )
-    {
-        set(m0, m1, m2, m3);
-    }
+                    const BMatrix3 &m2, const BMatrix3 &m3 ) :  m_data 
+                                                                {
+                                                                    m0[0][0], m0[0][1], m0[0][2], m1[0][0], m1[0][1], m1[0][2],
+                                                                    m0[1][0], m0[1][1], m0[1][2], m1[1][0], m1[1][1], m1[1][2],
+                                                                    m0[2][0], m0[2][1], m0[2][2], m1[2][0], m1[2][1], m1[2][2],
+
+                                                                    m2[0][0], m2[0][1], m2[0][2], m3[0][0], m3[0][1], m3[0][2],
+                                                                    m2[1][0], m2[1][1], m2[1][2], m3[1][0], m3[1][1], m3[1][2],
+                                                                    m2[2][0], m2[2][1], m2[2][2], m3[2][0], m3[2][1], m3[2][2],
+                                                                } {}
     
     ~BSpatialMatrix( void )=default;
 
