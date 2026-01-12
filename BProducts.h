2f9e678afe5b3744a70e44fb26e68feed426fe5d
wbyates777 <wbyates777@gmail.com>
2026-01-12 02:39:43 +0100

    Products declared constexpr
    Also added the tested block definitions of arb::crossm and arb::crossf
    Gives users a choice of implementation

--- BProducts.h
+++ BProducts.h
@@ -1,4 +1,4 @@
-/* BProducts 11/06/2025
+/* Spatial Cross Products 11/06/2025
 
  $$$$$$$$$$$$$$$$$$$
  $   BProducts.h   $
@@ -9,9 +9,10 @@
  History:
 
  
- Spatial Cross Products (see Featherstone, RBDA, Section 2.9, page 23),
- and any other product operators in one handy file. 
+ Spatial Cross Products (see Featherstone, RBDA, Section 2.9, page 23). 
+
  
+ All cross product operators in one handy file 
  These get used a lot -- good candidate for SIMD optimisation
 
 */
@@ -27,34 +28,33 @@
 namespace arb
 {
 
-    // standard Euclidian cross product 
-    inline const BVector3
-    cross( const BVector3 &a, const BVector3 &b )
+    // standard Euclidian cross product a⨉b
+    inline constexpr BVector3   
+    cross( const BVector3 &x, const BVector3 &y )
     {
-        return glm::cross(a, b);
-        
-        //return BVector3( a[1] * b[2] - b[1] * a[2],
-        //                 a[2] * b[0] - b[2] * a[0],
-        //                 a[0] * b[1] - b[0] * a[1] );
+        return glm::cross(x, y);
+        //return BVector3( x[1] * y[2] - y[1] * x[2],
+        //                 x[2] * y[0] - y[2] * x[0],
+        //                 x[0] * y[1] - y[0] * x[1] );
     }
   
 
     // The Euclidian cross product can be written as an equivalent matrix multiplication
     // The operator $v\times$ (pronounced ‘v-cross’) constructs a skew-symmetric matrix $m$ from vector $v$  
-    // such that $m * a = v \times a$, see RBDA, Section 2.8, eqn 2.23, page 21
+    // such that $m * a = v \times a$, (see RBDA, Section 2.8, eqn 2.23, page 21 and table 2.1, page 22).
     // note arb::cross(-v) = arb::transpose(arb::cross(v))
     // implemented in RDBL as VectorCrossMatrix in SpatialAlgebraOperators.h
-    // see https://en.wikipedia.org/wiki/Skew-symmetric_matrix
-    inline const BMatrix3 
+    // see Tensor section  in https://en.wikipedia.org/wiki/Angular_velocity
+    inline constexpr BMatrix3 
     cross( const BVector3 &v ) 
     {
         return BMatrix3(  0.0, -v[2],  v[1],
                          v[2],   0.0, -v[0],
                         -v[1],  v[0],   0.0 );
     }
-     
+ 
     // return arb::cross(v) * arb::cross(-v)
-    inline const BMatrix3 
+    inline constexpr BMatrix3 
     crosst( const BVector3 &v ) 
     {
         const BScalar xx =  v[0] * v[0];
@@ -64,16 +64,16 @@ namespace arb
         const BScalar xz =  v[0] * v[2];
         const BScalar yz =  v[1] * v[2];
 
-        return BMatrix3( zz + yy,    -xy,      -xz,
-                           -xy,    zz + xx,    -yz,
-                           -xz,      -yz,    yy + xx );
+        return BMatrix3( yy + zz,    -xy,      -xz,
+                           -xy,    xx + zz,    -yz,
+                           -xz,      -yz,    xx + yy );
 
     }
 
   
     // spatial cross product for 'motion cross motion' vectors $v$ and $m$
     // such that $\dot{m} = v \times m$, see RBDA, Section 2.9, eqn 2.29, page 23
-    inline const BVector6 
+    inline constexpr BVector6 
     crossm( const BVector6 &v, const BVector6 &m ) 
     {
         return BVector6( -v[2] * m[1] + v[1] * m[2],
@@ -82,11 +82,19 @@ namespace arb
                          -v[5] * m[1] + v[4] * m[2] - v[2] * m[4] + v[1] * m[5],
                           v[5] * m[0] - v[3] * m[2] + v[2] * m[3] - v[0] * m[5],
                          -v[4] * m[0] + v[3] * m[1] - v[1] * m[3] + v[0] * m[4] );
+        
+
+       /* const BVector3 v_ang = v.ang(); 
+        const BVector3 v_lin = v.lin(); 
+        const BVector3 m_ang = m.ang(); 
+        const BVector3 m_lin = m.lin(); 
+
+        return BVector6(arb::cross(v_ang, m_ang), arb::cross(v_ang, m_lin) + arb::cross(v_lin, m_ang));*/
     }
 
     // spatial cross product for 'motion cross force' vectors $v$ and $f$
     // such that $\dot{f} = v \times^* f$, see RBDA, Section 2.9, eqn 2.30, page 23
-    inline const BVector6 
+    inline constexpr BVector6 
     crossf( const BVector6 &v, const BVector6 &f ) 
     {
         return BVector6( -v[2] * f[1] + v[1] * f[2] - v[5] * f[4] + v[4] * f[5],
@@ -95,11 +103,19 @@ namespace arb
                          -v[2] * f[4] + v[1] * f[5],
                           v[2] * f[3] - v[0] * f[5],
                          -v[1] * f[3] + v[0] * f[4] );
+        
+     /*   const BVector3 v_ang = v.ang(); 
+        const BVector3 v_lin = v.lin(); 
+        const BVector3 f_ang = f.ang(); 
+        const BVector3 f_lin = f.lin(); 
+
+        return BVector6(arb::cross(v_ang, f_ang) + arb::cross(v_lin, f_lin), arb::cross(v_ang, f_lin));*/
     }
 
 
     // motion vector operator $v\times$, see RBDA, Section 2.9, eqn 2.31, page 25
-    inline const BMatrix6 
+    // WBY unused so far
+    inline constexpr BMatrix6 
     crossm( const BVector6 &v ) 
     {
         //return BMatrix6(arb::cross(v.ang()), B_ZERO_3x3, arb::cross(v.lin()), arb::cross(v.ang()));
@@ -107,12 +123,13 @@ namespace arb
                         v[2],    0.0,  -v[0],   0.0,   0.0,    0.0, 
                        -v[1],   v[0],    0.0,   0.0,   0.0,    0.0,        
                          0.0,  -v[5],   v[4],   0.0, -v[2],   v[1],
-                        v[5],    0.0,  -v[3],   v[2],  0.0,  -v[0],   
-                       -v[4],   v[3],    0.0,  -v[1], v[0],    0.0 );
+                        v[5],    0.0,  -v[3],  v[2],   0.0,  -v[0],   
+                       -v[4],   v[3],    0.0, -v[1],  v[0],    0.0 );
     }
 
     // force vector operator $v\times^*$, see RBDA, Section 2.9, eqn 2.32, page 25
-    inline const BMatrix6 
+    // WBY unused so far  
+    inline constexpr BMatrix6 
     crossf( const BVector6 &v ) 
     {
         //return BMatrix6(arb::cross(v.ang()), arb::cross(v.lin()), B_ZERO_3x3, arb::cross(v.ang()));
@@ -124,48 +141,49 @@ namespace arb
                          0.0,   0.0,   0.0,  -v[1],   v[0],    0.0 );
     }
 
-    // standard dot or inner product
+    // standard dot or inner product  a·b  
     inline BScalar 
-    dot(const BVector3 &a, const BVector3 &b)
+    dot(const BVector3 &v1, const BVector3 &v2) 
     {
-        return glm::dot(a, b);
-        //return (a[0] * b[0]) + (a[1] * b[1]) + (a[2] * b[2]);
-    }
-
-    // spatial dot or inner product
-    inline BScalar 
-    dot(const BVector6 &a, const BVector6 &b)
-    {
-        return glm::dot(a.ang(), b.ang()) + glm::dot(a.lin(), b.lin());
-        //return (a[0] * b[0]) + (a[1] * b[1]) + (a[2] * b[2]) 
-        //        + (a[3] * b[3]) + (a[4] * b[4]) + (a[5] * b[5]);
+        return glm::dot(v1, v2);
+        //return (v1[0] * v2[0]) + (v1[1] * v2[1]) + (v1[2] * v2[2]);
     }
     
-    // outer product 
-    inline const BMatrix3
-    outer( const BVector3 &a, const BVector3 &b )
-    { 
-        return glm::outerProduct(b, a); // note signature is glm::outerProduct(col, row)
-        //return BMatrix3(a[0] * b[0], a[0] * b[1], a[0] * b[2], 
-        //                a[1] * b[0], a[1] * b[1], a[1] * b[2],  
-        //                a[2] * b[0], a[2] * b[1], a[2] * b[2] );
+    // spatial dot or inner product  a·b
+    inline BScalar 
+    dot(const BVector6 &v1, const BVector6 &v2)
+    {
+        return glm::dot(v1.ang(), v2.ang()) + glm::dot(v1.lin(), v2.lin());
+        //return (v1[0] * v2[0]) + (v1[1] * v2[1]) + (v1[2] * v2[2]) 
+        //        + (v1[3] * v2[3]) + (v1[4] * v2[4]) + (v1[5] * v2[5]);
     }
 
-    // spatial outer product 
-    inline const BMatrix6
+
+    inline constexpr BMatrix3  // Euclidian outer product a⨂b
+    outer( const BVector3 &a, const BVector3 &b )
+    { 
+        return glm::outerProduct(b,a);
+       // return BMatrix3(a[0] * b[0], a[0] * b[1], a[0] * b[2], 
+       //                 a[1] * b[0], a[1] * b[1], a[1] * b[2],  
+       //                 a[2] * b[0], a[2] * b[1], a[2] * b[2] );
+    }
+
+    // spatial outer product a⨂b
+    inline constexpr BMatrix6
     outer( const BVector6 &a, const BVector6 &b )
     { 
         return BMatrix6(a[0] * b[0], a[0] * b[1], a[0] * b[2],  a[0] * b[3], a[0] * b[4], a[0] * b[5], 
                         a[1] * b[0], a[1] * b[1], a[1] * b[2],  a[1] * b[3], a[1] * b[4], a[1] * b[5], 
                         a[2] * b[0], a[2] * b[1], a[2] * b[2],  a[2] * b[3], a[2] * b[4], a[2] * b[5], 
-                    
+                        
                         a[3] * b[0], a[3] * b[1], a[3] * b[2],  a[3] * b[3], a[3] * b[4], a[3] * b[5], 
                         a[4] * b[0], a[4] * b[1], a[4] * b[2],  a[4] * b[3], a[4] * b[4], a[4] * b[5], 
                         a[5] * b[0], a[5] * b[1], a[5] * b[2],  a[5] * b[3], a[5] * b[4], a[5] * b[5]);
-
-        // return BMatrix6( glm::outerProduct(b.ang(), a.ang()), glm::outerProduct(b.lin(), a.ang()),
-        //                        glm::outerProduct(b.ang(), a.lin()), glm::outerProduct(b.ang(), a.ang()) );
+        
+         //return BMatrix6( glm::outerProduct(b.ang(), a.ang()), glm::outerProduct(b.lin(), a.ang()),
+          //                glm::outerProduct(b.ang(), a.lin()), glm::outerProduct(b.ang(), a.ang()) );
     }
+
 }
 
 
