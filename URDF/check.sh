24c173e98bac3669fac8faaaed9f4e89a82b0599
wbyates777 <wbyates777@gmail.com>
2026-04-21 16:40:17 +0200

    Delete URDF/check.sh

--- URDF/check.sh
+++
@@ -1,7 +0,0 @@
-if [ "$#" -ne 1 ]; then
-    echo "Usage: $0 <dir>"
-    exit 1
-fi
-#cppcheck --library=std --check-level=exhaustive --inconclusive --enable=all --suppress=missingIncludeSystem --safety --checkers-report=error.txt --std=c++20 -v $1
-
-cppcheck -I/Users/bill/Projects/src/Graphics/Libs/glm --library=std --suppress=missingIncludeSystem --check-level=exhaustive --inconclusive --enable=all --safety --checkers-report=error.txt --std=c++20 -v $1
