8c90b01d88edcd56784f6b0ec076dba109c0e788
wbyates777 <wbyates777@gmail.com>
2026-09-08 14:17:36 +0200

    Fix for autodiff
    My new optimisation does not work with Autodiff. This is an emergency fix until I have sorted the issue.
    Double types use the optimisation, autodiff does not.
    This time I have checked in the correct version.

--- SpatialAlgebra/BFunctions.h
+++ SpatialAlgebra/BFunctions.h
@@ -91,6 +91,8 @@ namespace arb {
     }
     
     // 3D rotations - all angles in radians
+
+#if defined(ARB_USE_AUTODIFF)
     
     inline constexpr BMatrix3 
     rot( BScalar theta, const BVector3 &axis ) 
@@ -103,20 +105,35 @@ namespace arb {
         
         const BScalar s = sin(theta);
         const BScalar c = cos(theta);
-  
-        return BMatrix3( axis[0] * axis[0] * (1.0 - c) + c,
-                         axis[1] * axis[0] * (1.0 - c) + axis[2] * s,
-                         axis[0] * axis[2] * (1.0 - c) - axis[1] * s,
+        const BScalar omc = (1.0 - c);
+        
+        return BMatrix3( axis[0] * axis[0] * omc + c,
+                         axis[1] * axis[0] * omc + axis[2] * s,
+                         axis[0] * axis[2] * omc - axis[1] * s,
                       
-                         axis[0] * axis[1] * (1.0 - c) - axis[2] * s,
-                         axis[1] * axis[1] * (1.0 - c) + c,
-                         axis[1] * axis[2] * (1.0 - c) + axis[0] * s,
+                         axis[0] * axis[1] * omc - axis[2] * s,
+                         axis[1] * axis[1] * omc + c,
+                         axis[1] * axis[2] * omc + axis[0] * s,
                       
-                         axis[0] * axis[2] * (1.0 - c) + axis[1] * s,
-                         axis[1] * axis[2] * (1.0 - c) - axis[0] * s,
-                         axis[2] * axis[2] * (1.0 - c) + c);
+                         axis[0] * axis[2] * omc + axis[1] * s,
+                         axis[1] * axis[2] * omc - axis[0] * s,
+                         axis[2] * axis[2] * omc + c);
     }
-    
+     
+#else
+     
+     inline constexpr BMatrix3 
+     rot( BScalar theta, const BVector3 &axis ) 
+     // WARNING: axis *must* be normalized
+     {
+         using std::abs;
+         using std::sin;
+         using std::cos;
+         assert(abs(arb::length(axis) - 1.0) < 1E-8);
+
+         return glm::mat3_cast(glm::angleAxis(theta, axis));
+     }   
+#endif
     
     inline constexpr BMatrix3 
     rotx( BScalar theta ) 
